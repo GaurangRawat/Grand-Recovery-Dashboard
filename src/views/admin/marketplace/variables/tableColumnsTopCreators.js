42cc58d4eefeca2583330c20e2aa13cc0b26fc80
GaurangRawat <Gaurangrawat2003@gmail.com>
2024-07-30 13:36:48 +0530

    grand dash
    Grand Recovery Dashboard

---
+++ src/views/admin/marketplace/variables/tableColumnsTopCreators.js
@@ -0,0 +1,14 @@
+export const tableColumnsTopCreators = [
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Plants Bought",
+    accessor: "Plants Bought",
+  },
+  {
+    Header: "Rating",
+    accessor: "rating",
+  },
+];
